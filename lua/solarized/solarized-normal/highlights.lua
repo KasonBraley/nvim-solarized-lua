ef5055aa272d9e681eb2a391c8064976de3d770d
kason <kbraley9@gmail.com>
2021-11-24 00:51:55 -0700

    Remove red brackets

--- lua/solarized/solarized-normal/highlights.lua
+++ lua/solarized/solarized-normal/highlights.lua
@@ -129,7 +129,7 @@ function M.load_syntax(colors)
 	syntax['Identifier'] = {fg=colors.blue}
 	syntax['Ignore'] = {fg=colors.none,ctermfg=colors.none,ctermbg=colors.none}
 	syntax['PreProc'] = {fg=colors.orange}
-	syntax['Special'] = {fg=colors.orange}
+	syntax['Special'] = {fg=colors.none}
 	syntax['Statement'] = {fg=colors.green}
 	syntax['Todo'] = {fg=colors.magenta,style='bold'}
 	syntax['Type'] = {fg=colors.yellow}
